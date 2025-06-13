17ab813c22844e578e485e8c877fb07eb4644bfb
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2025-06-13 19:22:11 -0400

    refactor: move frontend code to frontend directory

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+import { theme } from '@tailwindcss/theme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme,
+  plugins: [],
+}
